aea84847b5273e95e6f9793203220b5e9f06263e
GuiLSDias <glsd273@gmail.com>
2024-10-08 22:00:13 -0300

    Commit

--- src/javassortaula/JavasSortAula.java
+++ src/javassortaula/JavasSortAula.java
@@ -2,6 +2,7 @@
 package javassortaula;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Random;
 
@@ -27,7 +28,7 @@ public class JavasSortAula {
     }
 
     public static void main(String[] args) {
-        Pessoa[] vetorPessoas = geraArrayPessoas();
+        /*Pessoa[] vetorPessoas = geraArrayPessoas();
         System.out.println("Pessoas do vetor:");
         for(Pessoa p: vetorPessoas)
             System.out.println(p);
@@ -42,12 +43,13 @@ public class JavasSortAula {
         SelectionSort<Pessoa> ssort = new SelectionSort<>();
         InsertionSort<Pessoa> isort = new InsertionSort<>();
         //bsort.sort(vetorPessoas, comparaDataNascimento);
-        isort.sort(vetorPessoas,comparaDataNascimento);
+        //isort.sort(vetorPessoas,comparaDataNascimento);
+        Arrays.sort(vetorPessoas);
         System.out.println("Pessoas ordenadas pelo nome:");
         for(Pessoa p: vetorPessoas)
             System.out.println(p);
        
-       /*int tamanho = 50000, min = 1, max =100;
+       */int tamanho = 10, min = 1, max =10000;
        Integer[] vetor1 = geraArrayAleatorio(tamanho,min,max);
        Integer[] vetor2 = new Integer[tamanho];
        Integer[] vetor3 = new Integer[tamanho];
@@ -57,7 +59,7 @@ public class JavasSortAula {
            vetor3[i] = vetor1[i];
        }// copia do vetor 1 para os outros 2
        
-      /* Comparator<Integer> comparatorInteiro = (a,b)-> a.compareTo(b);
+      Comparator<Integer> comparatorInteiro = (a,b)-> a.compareTo(b);
        
        BubbleSort<Integer> bsort = new BubbleSort<>();
        SelectionSort<Integer> ssort = new SelectionSort<>();
@@ -67,21 +69,21 @@ public class JavasSortAula {
        bsort.sort(vetor1, comparatorInteiro);
        long tfinal = System.currentTimeMillis();
        long tempo = tfinal - inicio;
-        System.out.println("Tempo do Bubble Sort:"+tempo);
+        System.out.println("Tempo do Bubble Sort: "+tempo+" Comparações: "+bsort.getContaComparacoes());
        
         // Selection Sort
        inicio = System.currentTimeMillis();
        ssort.sort(vetor2, comparatorInteiro);
        tfinal = System.currentTimeMillis();
        tempo = tfinal - inicio;
-       System.out.println("Tempo do Selection Sort:"+tempo);
+       System.out.println("Tempo do Selection Sort: "+tempo+ " Comparações: "+ssort.getContaComparacao());
        
-       // Insertion Sort
+
        inicio = System.currentTimeMillis();
        isort.sort(vetor3, comparatorInteiro);
        tfinal = System.currentTimeMillis();
        tempo = tfinal - inicio;
-       System.out.println("Tempo do Insertion Sort:"+tempo);*/
+       System.out.println("Tempo do Insertion Sort: "+tempo+" Comparações: "+isort.getContaComparacoes());
     }
     
 }
--- src/javassortaula/SelectionSort.java
+++ src/javassortaula/SelectionSort.java
@@ -2,12 +2,19 @@
 package javassortaula;
 import java.util.Comparator;
 public class SelectionSort<T> {
-    private int indiceMenorValor(T[] v, int posicao,Comparator<T> comparador){
+    private long contaComparacao;
+
+    public long getContaComparacao() {
+        return contaComparacao;
+    }
+
+    private int indiceMenorValor(T[] v, int posicao, Comparator<T> comparador){
         int indiceMenor = posicao;
-        for(int i=posicao+1;i<v.length;i++)
-            if(comparador.compare(v[i], v[indiceMenor])<0)
+        for(int i=posicao+1;i<v.length;i++) {
+            this.contaComparacao++;
+            if (comparador.compare(v[i], v[indiceMenor]) < 0)
                 indiceMenor = i;
-        return indiceMenor;
+        } return indiceMenor;
     }
     //--------------------
     void troca(T[] v, int i, int j){
@@ -18,6 +25,7 @@ public class SelectionSort<T> {
     //--------------
     public void sort(T[] v, Comparator<T> comparador){
         int n= v.length;
+        this.contaComparacao = 0;
         for(int i=0;i<n-1;i++){ // for da fase
             int indiceMenor = indiceMenorValor(v, i, 
                               comparador);
