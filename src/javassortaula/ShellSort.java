18623dcd1d281339e9962910360ed63021a61ca2
GuiLSDias <glsd273@gmail.com>
2024-10-15 09:57:10 -0300

    Commit

--- src/javassortaula/ShellSort.java
+++ src/javassortaula/ShellSort.java
@@ -11,23 +11,18 @@ public class ShellSort<T> {
 
     public void sort(T[] v, Comparator<T> comparador) {
         int n = v.length;
-        contaComparacao = 0; // Zera o contador de comparações
+        contaComparacao = 0;
 
-        // Gap sequence (reduz pela metade em cada iteração)
+
         for (int gap = n / 2; gap > 0; gap /= 2) {
-            // Inserção por intervalos de "gap"
             for (int i = gap; i < n; i++) {
-                // Armazena o elemento a ser posicionado
                 T temp = v[i];
                 int j;
-
-                // Compara elementos com base no gap
                 for (j = i; j >= gap && comparador.compare(v[j - gap], temp) > 0; j -= gap) {
-                    v[j] = v[j - gap]; // Move o elemento para a frente se maior
-                    contaComparacao++;  // Incrementa o contador de comparações
+                    v[j] = v[j - gap];
+                    contaComparacao++;
                 }
-
-                v[j] = temp; // Coloca o elemento na posição correta
+                v[j] = temp;
             }
         }
     }
